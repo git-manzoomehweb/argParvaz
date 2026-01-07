d193900b4046e6d7b96bcedf87f9f14486e0da7b
fatmhJamali <kmg.d103@gmail.com>
2026-01-07 13:48:15 +0330

    article list

--- web/assets/js/argeparvaz-core.js
+++ web/assets/js/argeparvaz-core.js
@@ -316,6 +316,19 @@ if (document.querySelector(".show-more-text")) {
 }
 // ____________________________
 // ____________________________
+const clickerUl = document.querySelectorAll("ul.clicker-list");
+clickerUl?.forEach((el) => {
+  const liItem = el.querySelectorAll("li");
+  liItem[0].classList.add("active");
+  liItem.forEach((li) => {
+    li.addEventListener("click", () => {
+      liItem.forEach((element) => {
+        element.classList.remove("active");
+      });
+      li.classList.add("active");
+    });
+  });
+});
 // ____________________________
 // ____________________________
 // ____________________________
